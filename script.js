059738352471252f181dc8833bfe6bf3569fb821
babar <syedrazatx@gmail.com>
2024-02-26 16:07:23 -0600

    added comments

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const API_KEY = "";
 const output = document.getElementById("output");
 document.getElementById("models").addEventListener("change", show_options);
 
+// Toggle form fields depending on model selected
 function show_options(event) {
   const value = event.target.value;
-
   if (value === "gpt-4-turbo-preview" || value === "gpt-3.5-turbo-0125") {
     showGPTOptions();
   } else if (value === "dall-e-3") {
@@ -18,6 +18,7 @@ function show_options(event) {
   }
 }
 
+// Toggle form fields depending on model selected
 function showGPTOptions() {
   $("#dalle_options, #info, #vision_options, #tts_options, #player").hide();
   $("#output").show();
@@ -45,6 +46,7 @@ function showDalleOptions() {
   $("#uploaded_image").removeAttr("required");
 }
 
+// Form Submit Function that prepares api call with model specific data and calls API
 function runAI(event) {
   event.preventDefault();
   const prompt = $("#prompt");
@@ -122,12 +124,14 @@ function runAI(event) {
   show_loading();
 }
 
+// API Function
 async function api_call(options, endpoint, chosen_model) {
   try {
     const response = await fetch(`https://api.openai.com/v1/${endpoint}`, options);
+    // if model is not text-to-speech
     if (chosen_model != "tts-1") {
       const result = await response.json();
-      await display_result(result, chosen_model);
+      display_result(result, chosen_model);
       console.log(
         "%c Result -",
         "font-weight:bold",
@@ -135,8 +139,9 @@ async function api_call(options, endpoint, chosen_model) {
         result
       );
     } else {
+      // text-to-speech endpoint returns audio
       const result = await response.blob();
-      await display_result(result, chosen_model);
+      display_result(result, chosen_model);
     }
   } catch (error) {
     console.error("An error occurred:", error);
@@ -144,6 +149,7 @@ async function api_call(options, endpoint, chosen_model) {
   }
 }
 
+// styles the <code> block with a header containing language name and copy button
 function add_header_to_code_block() {
   if ($("code").length) {
     const lang = $("code").attr("class").split(" ")[0].split("-")[1];
@@ -151,10 +157,12 @@ function add_header_to_code_block() {
     let header_div = `<div class="header_div"><div>${lang}</div><div class="copyBtn" onclick="copyCode()">copy</div></div>`;
     $(header_div).prependTo("pre");
   } else {
+    // debug log
     console.log("response doesnt include <code> tags");
   }
 }
 
+// helper function to display the result depending on the model chosen
 function display_result(result, chosen_model) {
   close_loading();
   $(".output_area").show();
@@ -165,29 +173,36 @@ function display_result(result, chosen_model) {
     chosen_model === "gpt-4-vision-preview"
   ) {
     output.innerHTML = marked.parse(result.choices[0].message.content);
+    // colorize <code> block
     hljs.highlightAll();
+    // style <code> block
     add_header_to_code_block();
   } else if (chosen_model === "dall-e-3") {
+    // show generated image
     output.innerHTML = marked.parse(`![Generated Image](${result.data[0].url})`);
     $("#info").html(
       `<a href="${result.data[0].url}">Full Size Image</a><br><br><b>Revised Prompt:</b><br>${result.data[0].revised_prompt}`
     );
   } else if (chosen_model === "tts-1") {
     var url = URL.createObjectURL(result);
+    // show audio player
     $("#player").attr("src", url);
   }
 }
 
+// show loading animation
 function show_loading() {
   $("#loading_main").show();
   output.classList.add("placeholder");
 }
 
+// hide loading animation
 function close_loading() {
   $("#loading_main").hide();
   output.classList.remove("placeholder");
 }
 
+// preview user selected image in form for GPT4 Vision
 function previewImage(event) {
   const reader = new FileReader();
   reader.onload = function () {
@@ -198,15 +213,24 @@ function previewImage(event) {
   reader.readAsDataURL(event.target.files[0]);
 }
 
+// toggle dark mode
 document.getElementById("btnSwitch").addEventListener("click", () => {
   const currentTheme = document.documentElement.getAttribute("data-bs-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   document.documentElement.setAttribute("data-bs-theme", newTheme);
 });
 
+// button in <code> block header to allow user to copy code
+function copyCode() {
+  var codeBlock = document.querySelector("code");
+  navigator.clipboard.writeText(codeBlock.innerText);
+}
+
+// users usage data (tokens and cost) for openai api for given api key for the day
 async function get_usage() {
   try {
     const formattedDate = new Date().toISOString().slice(0, 10);
+    // get usage data for todays date
     const response = await fetch(`https://api.openai.com/v1/usage?date=${formattedDate}`, {
       method: "GET",
       headers: {
@@ -214,8 +238,9 @@ async function get_usage() {
         Authorization: `Bearer ${API_KEY}`,
       },
     });
-    if (!response.ok) throw new Error("Failed to fetch usage data");
+    if (!response.ok) throw new Error("Failed to fetch usage data, make sure API key is set");
     const result = await response.json();
+    // debug log
     console.log(
       "%c Usage checked at",
       "font-weight:bold",
@@ -229,11 +254,7 @@ async function get_usage() {
   }
 }
 
-function copyCode() {
-  var codeBlock = document.querySelector("code");
-  navigator.clipboard.writeText(codeBlock.innerText);
-}
-
+// convert api usage to dollar cost
 function calculateTotalUsage(data) {
   const prices = {
     "gpt-3.5-turbo-0125": {
@@ -255,12 +276,11 @@ function calculateTotalUsage(data) {
       speech: 0.015,
     },
   };
-  // Initialize the total usage as zero
 
+  // initialize the total usage as zero
   let totalUsage = 0;
 
-  // Initialize the variables for the input tokens, output tokens, requests, images, and characters for each model and operation
-
+  // usage variables for each model
   let gpt3 = {
     n_context_tokens_total: 0,
     n_generated_tokens_total: 0,
@@ -280,24 +300,24 @@ function calculateTotalUsage(data) {
     totalCharacters: 0,
   };
 
-  // Loop through the data arrays for each model and operation
+  // Loop through the data arrays for each model
   for (let key in data) {
     // Skip the non-array properties
     if (!Array.isArray(data[key])) continue;
 
     // Loop through the objects in each array
     for (let obj of data[key]) {
-      // Get the model id and the operation from the object
+      // Get the type of model used from the object
       let modelId = obj.model_id || obj.snapshot_id;
 
-      // Get the values of the input tokens, output tokens, requests, images, and characters from the object
+      // get the relevant values from the object
       let inputTokens = obj.n_context_tokens_total || 0;
       let outputTokens = obj.n_generated_tokens_total || 0;
       let images = obj.num_images || 0;
       let characters = obj.num_characters || 0;
       let cost = 0;
 
-      // Update the variables for the input tokens, output tokens, requests, images, and characters for each model and operation
+      // update the variables for each model
       if (modelId === "gpt-4-0125-preview") {
         gpt4.n_context_tokens_total += inputTokens;
         gpt4.n_generated_tokens_total += outputTokens;
@@ -326,13 +346,13 @@ function calculateTotalUsage(data) {
             outputTokens * prices[modelId].n_generated_tokens_total) /
           1000;
       }
-
       totalUsage += cost;
     }
   }
+
+  // total cost for the day
   totalUsage = totalUsage.toFixed(2);
 
-  // Return the total usage and the variables for the input tokens, output tokens, requests, images, and characters for each model and operation
   return {
     totalUsage,
     gpt3,
@@ -343,6 +363,7 @@ function calculateTotalUsage(data) {
   };
 }
 
+// format the usage data
 function showUsage(usage) {
   document.getElementById("usage").innerHTML = `
                           GPT 3: Input: ${usage.gpt3.n_context_tokens_total} / Generated: ${usage.gpt3.n_generated_tokens_total}<br>
